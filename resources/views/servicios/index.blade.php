e2de2624bd120c5e9f4864cf9940811b727aa448
danigallard0 <dani10gallardo95@gmail.com>
2026-05-02 17:15:17 +0200

    Cambiar título por logo en servicios

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -11,8 +11,10 @@
 
     <header class="bg-white shadow-sm">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-            <a href="{{ url('/') }}" class="text-2xl font-bold text-pink-700">
-                Tamara Hoyas Beauty Studio
+            <a href="{{ url('/') }}" class="flex items-center gap-2">
+                <img src="{{ asset('logo.png') }}"
+                    alt="Tamara Hoyas Beauty Studio"
+                    class="h-8 sm:h-10 w-auto object-contain">
             </a>
 
             <a href="{{ url('/') }}"
