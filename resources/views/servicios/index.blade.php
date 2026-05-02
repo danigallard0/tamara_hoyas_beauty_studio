b48ce4184d2a4734a5e07c416bbab9f67b2e3d07
danigallard0 <dani10gallardo95@gmail.com>
2026-05-02 17:11:47 +0200

    Añadir favicon en páginas de servicios

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-app.png') }}?v=2">
     <title>Servicios - Tamara Hoyas Beauty Studio</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
